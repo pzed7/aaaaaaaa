de5329c06ef9f89634298aabac9ace56ba5b751b
pzed7 <pejvak.zamani12345@gmail.com>
2022-03-06 01:42:50 +0330

    sd

--- main.js
+++ main.js
@@ -18,4 +18,4 @@ global.player = new Player(client, client.config.opt.discordPlayer);
 require('./src/loader');
 require('./src/events');
 
-client.login("OTQ4OTgyMzI2OTUwNDk0MzE5.YiDuqQ.BAfwracqM_0kw-4N2dwG2Pk-6pI");
+client.login(process.env.DISCORD_BOT_TOKEN);
